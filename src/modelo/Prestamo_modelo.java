afc096f647539d91834a7239b55102c0708a77c6
Iker <ikaslea@10.22.72.143>
2017-02-23 12:51:27 +0100

    Prestamo modelo modificado.

--- src/modelo/Prestamo_modelo.java
+++ src/modelo/Prestamo_modelo.java
@@ -31,13 +31,25 @@ public class Prestamo_modelo extends Conector{
 	public Prestamo select(int id_libro, int id_socio, Date fecha){
 		try{
 			Statement st = this.conexion.createStatement();
-			ResultSet rs = st.executeQuery("select * from prestamos where id=" + id_libro);
+			ResultSet rs = st.executeQuery("select * from prestamos where id_libro=" + id_libro + "AND id_socio=" + id_socio + "AND fecha=" + fecha);
+			while(rs.next()){
+				if (id_libro==rs.getInt("id_libro")){
+					}else if(id_socio==rs.getInt("id_socio")){
+						}else if(fecha==rs.getDate("fecha")){
+							return new Prestamo(rs.getInt("id_libro"), rs.getInt("id_socio"), rs.getDate("fecha"), rs.getBoolean("devuelto"));	
+				}
+			}
 			
 		}catch (SQLException e){
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public void insert (Prestamo prestamo){
+		
+	}
 		
 		
 	}
-}
+
